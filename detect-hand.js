a04d1204276305a419447234285e9c47040c619c
silvererudite <shamima2hossain@gmail.com>
2021-12-31 02:04:41 +0600

    injected canvas with a placeholder drawing

---
+++ detect-hand.js
@@ -0,0 +1,8 @@
+// const btn = document.getElementById('prepare-canvas');
+
+// btn.addEventListener('click', (tab) => {
+//     chrome.scripting.executeScript({
+//       target: { tabId: tab.id },
+//       files: ['content-script.js']
+//     });
+//   })
